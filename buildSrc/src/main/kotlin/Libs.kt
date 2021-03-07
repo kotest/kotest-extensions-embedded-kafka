2a8497d366683ca43b26dee809eb5e70ee7c3529
sksamuel <sam@sksamuel.com>
2021-03-07 14:31:10 -0600

    Added actions

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,22 @@
+object Libs {
+
+   const val kotlinVersion = "1.4.31"
+   const val org = "io.kotest.extensions"
+
+   object Kotlin {
+      const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
+      const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+   }
+
+   object Kotest {
+      private const val version = "4.4.2"
+      const val assertions = "io.kotest:kotest-assertions-core-jvm:$version"
+      const val api = "io.kotest:kotest-framework-api:$version"
+      const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
+   }
+
+   object embeddedkafka {
+      private const val version = "2.7.0"
+      const val kafka = "io.github.embeddedkafka:embedded-kafka_2.13:$version"
+   }
+}
